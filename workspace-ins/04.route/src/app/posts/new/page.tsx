8a03c05fdeaf28f5de4c70dd0ff14d73232ac96f
Kilyong Jeong <uzoolove@gmail.com>
2024-07-23 11:40:07 +0900

    ✨

--- workspace-ins/04.route/src/app/posts/new/page.tsx
+++ workspace-ins/04.route/src/app/posts/new/page.tsx
@@ -1,5 +1,15 @@
+'use client';
+
+import { redirect, useRouter } from "next/navigation";
+
 export default function NewPage() {
+  const router = useRouter();
+  const isLogin = false;
+  if(!isLogin) redirect('/login');
   return (
-    <h1 className="text-xl font-bold mb-4">글쓰기</h1>
+    <>
+      <h1 className="text-xl font-bold mb-4">글쓰기</h1>
+      <button type="button" onClick={ () => router.push('/login') }>로그인</button>
+    </>
   );
 }
